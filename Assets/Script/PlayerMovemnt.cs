7dffafcc77a8cea38d3c3df7d00cb3147b651533
이형주 <jujumom630@gmail.com>
2025-05-29 11:19:32 +0900

    플레이어 움직임
    + 컴포지트 콜라이더 적용

--- Assets/Script/PlayerMovemnt.cs
+++ Assets/Script/PlayerMovemnt.cs
@@ -2,15 +2,42 @@ using UnityEngine;
 
 public class PlayerMovemnt : MonoBehaviour
 {
+    Rigidbody2D rb;
+    SpriteRenderer spriteRenderer;
+    public float speed = 10f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        FilpSprite();
+        MovePlayer();
     }
+
+    void FilpSprite()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        if (moveHorizontal > 0)
+        {
+            spriteRenderer.flipX = false;
+        }
+        else if (moveHorizontal < 0)
+        {
+            spriteRenderer.flipX = true;
+        }
+    }
+
+    void MovePlayer()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        Vector2 movement = rb.linearVelocity;
+        movement.x = moveHorizontal * speed;
+        rb.linearVelocity = movement;
+    }
+
 }
