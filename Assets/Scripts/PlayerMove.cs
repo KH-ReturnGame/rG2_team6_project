e3e350bcdda6747aee4693aebf4bd954c423545a
sanghane <sanghane08@gmail.com>
2025-06-07 17:25:57 +0900

    2025년도-6월-7일_과제_3
    sadfs

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -7,17 +7,28 @@ public class PlayerMove : MonoBehaviour
 
 
     public float speed = 5f;
+    float JumpPower = 10f;
     Vector2 move = new Vector2();
     Rigidbody2D rb;
+    Collider2D col;
+    public LayerMask groundLayer;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        col = GetComponent<Collider2D>();
     }
 
 
     void Update()
     {
-
+        bool isGrounded = col.IsTouchingLayers(groundLayer);
+            Debug.Log("IsGrounded" + isGrounded);
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            Debug.Log("스페이스바 누름");
+            rb.AddForce(Vector2.up * JumpPower, ForceMode2D.Impulse);
+            //플레이어 점프
+        }
     }
 
     private void FixedUpdate()
@@ -26,8 +37,21 @@ public class PlayerMove : MonoBehaviour
 
         //Debug.Log(Input.GetAxisRaw("Horizontal"));
         //Debug.Log(Input.GetAxis("Horizontal"));
-        
+        //이동할때 속도변화 보여줌
+
         move.Normalize();
-        rb.linearVelocity = move * speed;   
+        rb.linearVelocity = new Vector2(move.x * speed, rb.linearVelocity.y);
     }
+
+    /*IEnumerator logonetwo()
+    {
+        Debug.Log("One");
+        float waitTime = 1f;
+
+        yield return new WaitForSeconds(waitTime);
+
+        Debug.Log("Two");
+
+        yield return null;
+    }*/
 }
