7dbc133f475bd15a3827fb600ccb3bde5ac3094e
sanghane <sanghane08@gmail.com>
2025-07-04 11:52:03 +0900

    2025년도-7월-4일_과제_4.5
    ㅁ

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
-public class PlayerMove : MonoBehaviour
+public class Player : MonoBehaviour
 {
 
 
@@ -12,17 +12,31 @@ public class PlayerMove : MonoBehaviour
     Rigidbody2D rb;
     Collider2D col;
     public LayerMask groundLayer;
+
+
+    public int HP = 100;
+    private bool isInvincible = false; // 무적 여부
+    private float invincibleDuration = 1.5f; // 무적 시간
+
+
+
+
+
+
+
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         col = GetComponent<Collider2D>();
+        Debug.Log(HP);
     }
 
 
     void Update()
     {
         bool isGrounded = col.IsTouchingLayers(groundLayer);
-            //Debug.Log("IsGrounded" + isGrounded);
+        //Debug.Log("IsGrounded" + isGrounded);
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
             //Debug.Log("스페이스바 누름");
@@ -54,4 +68,45 @@ public class PlayerMove : MonoBehaviour
 
         yield return null;
     }*/
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Thorn"))
+        {
+            TakeDamage(10); // 함정에 닿으면 10 데미지
+        }
+    }
+
+    void TakeDamage(int damage)
+    {
+        HP -= damage;
+        Debug.Log("플레이어가 데미지를 입었습니다. 현재 HP: " + HP);
+
+        if (HP <= 0)
+        {
+            Die();
+        }
+        else
+        {
+            StartCoroutine(InvincibilityCoroutine());
+        }
+    }
+    System.Collections.IEnumerator InvincibilityCoroutine()
+    {
+        isInvincible = true;
+        Debug.Log("무적 상태 시작");
+
+        yield return new WaitForSeconds(invincibleDuration);
+
+        isInvincible = false;
+        Debug.Log("무적 상태 해제");
+    }
+
+    void Die()
+    {
+        Debug.Log("플레이어 사망");
+        //플레이어 죽음
+    }
+
+
 }
